aa5dc15ee56162ead6786c2e82181cfc7a86a13c
Kobe Michiels <kobe.michiels@outlook.com>
2024-02-01 20:30:09 +0100

    Created abstract BoardElement class

---
+++ src/BoardElement.java
@@ -0,0 +1,25 @@
+public abstract class BoardElement {
+    protected ElementType elementType;
+    protected boolean flag;
+    protected boolean visibility;
+
+    public BoardElement(ElementType elementType){
+        this.elementType = elementType;
+        this.flag = false;
+        this.visibility = false;
+    }
+    public ElementType getElementType() {
+        return elementType;
+    }
+    public boolean getVisibility() {
+        return visibility;
+    }
+    public void makeVisible(){visibility = true;}
+    public boolean getFlag() {
+        return flag;
+    }
+    public void addFlag(){flag = true;}
+    public void removeFlag(){flag = false;}
+
+
+}
