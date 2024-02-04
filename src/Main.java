a031e0e5b07e33679dc2d27fab74d689b78a0490
Kobe Michiels <kobe.michiels@outlook.com>
2024-02-04 17:56:54 +0100

    Created main function for executing the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import UserInteractionClasses.MinesweeperGUI;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            MinesweeperGUI minesweeper = new MinesweeperGUI();
+        });
     }
 }
