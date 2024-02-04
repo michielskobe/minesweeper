30e2e707477ba42d4398a6efe957931b356328b5
Kobe Michiels <kobe.michiels@outlook.com>
2024-02-04 17:55:44 +0100

    Created ButtonClickListener for game board tiles

---
+++ src/UserInteractionClasses/ButtonClickListener.java
@@ -0,0 +1,117 @@
+package UserInteractionClasses;
+
+import BoardElementClasses.BoardElement;
+import BoardElementClasses.ElementType;
+import GameBoardClasses.GameBoard;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ButtonClickListener implements MouseListener {
+    private final int x,y;
+    private final GameBoard gameBoard;
+    private final JButton[][] buttons;
+    public ButtonClickListener(int x, int y, GameBoard gameBoard, JButton[][] buttons){
+        this.x = x;
+        this.y = y;
+        this.gameBoard = gameBoard;
+        this.buttons = buttons;
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int buttonClicked = e.getButton();
+        if (buttonClicked == MouseEvent.BUTTON1) {
+            leftClickButtonAction(x, y);
+        } else if (buttonClicked == MouseEvent.BUTTON3) {
+            rightClickButtonAction(x, y);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void leftClickButtonAction(int x, int y){
+        BoardElement element = gameBoard.getBoard()[x][y];
+        JButton button = buttons[x][y];
+        if (!element.getFlag()){
+            element.makeVisible();
+            button.setBackground(Color.WHITE);
+            if (element.getElementType().equals(ElementType.MINE)){
+                button.setText("\uD83D\uDCA3");
+                JOptionPane.showMessageDialog(null, "GAME OVER");
+            }
+            else {
+                if (element.getValue() == 0){
+                    button.setText("");
+                    openEmptyTiles(x, y);
+                }
+                else {
+                    button.setText(String.valueOf(element.getValue()));
+                }
+            }
+            button.setEnabled(false);
+        }
+    }
+
+    public void rightClickButtonAction(int x, int y){
+        BoardElement element = gameBoard.getBoard()[x][y];
+        JButton button = buttons[x][y];
+        if (element.getFlag()){
+            element.removeFlag();
+            gameBoard.incrementNumberOfFlags();
+            button.setText("");
+        }
+        else {
+            if (gameBoard.getNumberOfFlags()>0) {
+                if (!element.getVisibility()){
+                    element.addFlag();
+                    gameBoard.decrementNumberOfFlags();
+                    button.setText("\uD83D\uDEA9");
+                    }
+                }
+            else {
+                JOptionPane.showMessageDialog(null, "No flags left");
+            }
+        }
+    }
+
+   public void openEmptyTiles(int x, int y){
+       BoardElement[][] board = gameBoard.getBoard();
+        for(int m = -1; m <= 1; m++) {
+            for (int n = -1; n <= 1; n++) {
+                try {
+                    if (board[x + m][y + n].getValue() == 0 && !board[x + m][y + n].getVisibility()) {
+                        board[x + m][y + n].makeVisible();
+                        buttons[x + m][y + n].setBackground(Color.WHITE);
+                        buttons[x + m][y + n].setText("");
+                        buttons[x + m][y + n].setEnabled(false);
+                        if (board[x + m][y + n].getFlag()) {
+                            board[x + m][y + n].removeFlag();
+                        }
+                        this.openEmptyTiles((x + m), (y + n));
+                    } else if (board[x + m][y + n].getElementType().equals(ElementType.NUMBER) && !board[x + m][y + n].getVisibility()) {
+                        board[x + m][y + n].makeVisible();
+                        buttons[x + m][y + n].setBackground(Color.WHITE);
+                        buttons[x + m][y + n].setText(String.valueOf(board[x + m][y + n].getValue()));
+                        buttons[x + m][y + n].setEnabled(false);
+                        if (board[x + m][y + n].getFlag()) {
+                            board[x + m][y + n].removeFlag();
+                        }
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        }
+    }
+}
