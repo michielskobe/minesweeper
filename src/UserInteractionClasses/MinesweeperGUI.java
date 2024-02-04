3ba5f4e7bdcb2ceeaaff1b64c3f0afe51e15c67c
Kobe Michiels <kobe.michiels@outlook.com>
2024-02-04 23:22:15 +0100

    Added window for difficulty selection

--- src/UserInteractionClasses/MinesweeperGUI.java
+++ src/UserInteractionClasses/MinesweeperGUI.java
@@ -4,25 +4,74 @@ import GameBoardClasses.GameBoard;
 import GameBoardClasses.GameDifficulty;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 public class MinesweeperGUI extends JFrame {
 
     public MinesweeperGUI(){
-        startGame(GameDifficulty.BEGINNER);
+        this.setTitle("Minesweeper");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 300);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        JPanel homePanel = new JPanel();
+        homePanel.setLayout(new BoxLayout(homePanel, BoxLayout.Y_AXIS));
+        this.getContentPane().add(homePanel);
+
+        JLabel titleLabel = new JLabel("Minesweeper");
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 50));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(titleLabel);
+
+        JLabel creditLabel = new JLabel("Implementation by Kobe Michiels");
+        creditLabel.setFont(new Font("SansSerif", Font.BOLD, 10));
+        creditLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(creditLabel);
+        homePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JLabel instructionLabel = new JLabel("Please select your difficulty level to start the game.");
+        instructionLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+        homePanel.add(instructionLabel);
+        instructionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JButton beginnerButton = new JButton("Beginner");
+        homePanel.add(beginnerButton);
+        beginnerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        beginnerButton.addActionListener(e -> {
+            startGame(GameDifficulty.BEGINNER);
+            remove(homePanel);
+        });
+
+        JButton intermediateButton = new JButton("Intermediate");
+        homePanel.add(intermediateButton);
+        intermediateButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        intermediateButton.addActionListener(e -> {
+            startGame(GameDifficulty.INTERMEDIATE);
+            remove(homePanel);
+        });
+
+        JButton expertButton = new JButton("Expert");
+        homePanel.add(expertButton);
+        expertButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        expertButton.addActionListener(e -> {
+            startGame(GameDifficulty.EXPERT);
+            remove(homePanel);
+        });
+
+        setVisible(true);
     }
 
     public void startGame(GameDifficulty difficulty){
         switch (difficulty) {
-            case BEGINNER -> {
-                setSize(500, 675);
-            }
-            case INTERMEDIATE -> {
-                setSize(900, 675);
-            }
-            case EXPERT -> {
-                setSize(1600, 675);
-            }
+            case BEGINNER -> setSize(500, 675);
+            case INTERMEDIATE -> setSize(900, 675);
+            case EXPERT -> setSize(1600, 675);
         }
         setTitle("Minesweeper");
         setLocationRelativeTo(null);
