7ef1efcaa5ec70e6239a86edead91cee153ba5d3
Kobe Michiels <kobe.michiels@outlook.com>
2024-02-04 17:57:21 +0100

    Implemented getValue function for Mine object

--- src/BoardElementClasses/Mine.java
+++ src/BoardElementClasses/Mine.java
@@ -3,4 +3,9 @@ public class Mine extends BoardElement {
     public Mine(ElementType elementType) {
         super(elementType);
     }
+
+    @Override
+    public int getValue() {
+        return -1;
+    }
 }
