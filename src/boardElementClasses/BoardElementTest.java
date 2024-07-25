b4f9df8fcf83d06762cf6f9abbebafa7707b7d15
Kobe <kobe.michiels@outlook.com>
2024-07-25 11:16:26 +0200

    Added unit tests for BoardElements

--- src/boardElementClasses/BoardElementTest.java
+++ src/boardElementClasses/BoardElementTest.java
@@ -24,31 +24,72 @@ public class BoardElementTest {
         assertFalse(number.getFlag());
     }
 
-    /** Test makeVisible()-function */
+    /** Test getElementType()-method for Number object */
     @Test
     public void test03() {
+        Number number = new Number(ElementType.NUMBER, 1);
+        assertEquals(ElementType.NUMBER, number.getElementType());
+    }
+
+    /** Test getElementType()-method for Mine object*/
+    @Test
+    public void test04() {
+        Mine mine = new Mine(ElementType.MINE);
+        assertEquals(ElementType.MINE, mine.getElementType());
+    }
+
+    /** Test getVisibility()-method */
+    @Test
+    public void test05() {
+        Number number = new Number(ElementType.NUMBER, 5);
+        assertTrue(number.getVisibility() == true || number.getVisibility() == false);
+    }
+
+    /** Test makeVisible()-method */
+    @Test
+    public void test06() {
         Number number = new Number(ElementType.NUMBER, 5);
         assertFalse(number.getVisibility());
         number.makeVisible();
         assertTrue(number.getVisibility());
     }
 
-    /** Test addFlag()-function */
+    /** Test getFlag()-method */
     @Test
-    public void test04() {
+    public void test07() {
+        Number number = new Number(ElementType.NUMBER, 5);
+        assertTrue(number.getFlag() == true || number.getFlag() == false);
+    }
+
+    /** Test addFlag()-method */
+    @Test
+    public void test08() {
         Number number = new Number(ElementType.NUMBER, 5);
         assertFalse(number.getFlag());
         number.addFlag();
         assertTrue(number.getFlag());
     }
 
-    /** Test removeFlag()-function */
+    /** Test removeFlag()-method */
     @Test
-    public void test05() {
+    public void test09() {
         Number number = new Number(ElementType.NUMBER, 5);
         number.addFlag();
         number.removeFlag();
         assertFalse(number.getFlag());
     }
 
+    /** Test getValue()-method for Mine Object*/
+    @Test
+    public void test10() {
+        Mine mine = new Mine(ElementType.MINE);
+        assertEquals(-1, mine.getValue());
+    }
+
+    /** Test getValue()-method for Number Object*/
+    @Test
+    public void test11() {
+        Number number = new Number(ElementType.NUMBER, 5);
+        assertEquals(5, number.getValue());
+    }
 }
